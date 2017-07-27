9fb13a4bb39c29ae060eeb8b3d64e24de8121c6e
z00127508 <z00127508@gmail.com>
2017-07-27 12:51:59 +0800

    cookie

--- src/main/resources/static/js/my.js
+++ src/main/resources/static/js/my.js
@@ -1,6 +1,9 @@
 $("#login").on('click', function() {
 	var username = $("#username").val();
-  	var password = $("#password").val();
+  	var password1 = $("#password").val();
+    var password =  hex_md5(password1);
+    alert(password);
+
   	var captcha = $("#captcha").val();
   	checkUser(username,password,captcha,0);
 });
@@ -17,9 +20,12 @@ $("#register").on("click",function(){
 $("#register_submit").on('click',function(){
     var username = $("#username_register").val();
     var mobile = $("#phone").val();
-    var password = $("#psw").val();
-	var password2 = $("#pswa").val();
+    var password11 = $("#psw").val();
+	var password22 = $("#pswa").val();
 	var email = $("#email").val();
+    var password =  hex_md5(password11);
+    var password2 =  hex_md5(password22);
+
 
 	if(username == "" || username == null){
         window.wxc.xcConfirm("用户名不能为空!", window.wxc.xcConfirm.typeEnum.warning);
@@ -236,3 +242,4 @@ function deleteclick(username){
         }
     });
 };
+
